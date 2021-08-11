0e2fec2da9b36a955d9a43f134d2bf3eb9da334c
unknown <khalilayyash@gmail.com>
2021-08-11 10:46:35 +0300

    auth

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -40,6 +40,6 @@ module.exports.login = (req, res) => {
         const {_id, name, genre} = req.user;
         const token = signToken(_id);
         res.cookie('access_token', token, {httpOnly: true, sameSite: true});
-        res.status(200).json({isAuthenticated: true, user: {name, genre,k:"sds"}});
+        res.status(200).json({isAuthenticated: true, user: {name, genre, _id}});
     }
 }
