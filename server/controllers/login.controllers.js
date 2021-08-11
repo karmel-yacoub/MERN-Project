2911b1a73e8f70e48c65d6115e6b9c0b91d5856d
unknown <khalilayyash@gmail.com>
2021-08-11 14:52:07 +0300

    commit

---
+++ server/controllers/login.controllers.js
@@ -0,0 +1,56 @@
+const {User} =require('../models/user.models')
+const passport = require('passport');
+const passportConfig = require('../passport');
+const JWT = require('jsonwebtoken');
+
+const signToken = userID => {
+    return JWT.sign({
+        iss: "khalil",
+        sub : userID,
+    }, "khalil", {expiresIn: "1h"});
+}
+
+// import controller Image saving in Database
+var fs = require('fs');
+var path = require('path');
+
+module.exports.createUser=(req,res) => {
+    const{name , email , password ,phone , location, genre , picture , menu} = req.body;
+    User.create({
+        name,
+        email,
+        password,
+        phone,
+        location,
+        genre,
+        // picture :{
+        //     data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+        //     contentType: 'image/png'
+        // },
+        menu,
+    })
+    .then(newUser => res.json(newUser))
+    .catch(err => res.json(err));
+}
+
+module.exports.login = (req, res) => {
+    console.log("login....")
+    console.log(req)
+    if(req.isAuthenticated()) {
+        const {_id, name, genre} = req.user;
+        const token = signToken(_id);
+        res.cookie('access_token', token, {httpOnly: true, sameSite: true});
+        res.status(200).json({isAuthenticated: true, user: {name, genre, _id}});
+    }
+}
+
+module.exports.logout = (req, res) => {
+    console.log("logout....")
+    // console.log(req)
+    if(req.isAuthenticated()) {
+        const {_id, name, genre} = req.user;
+        const token = signToken(_id);
+        res.cookie('access_token', token, {httpOnly: true, sameSite: true});
+        res.status(200).json({isAuthenticated: true, user: {name, genre, _id}});
+    }
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,13 @@
 const UserController = require('../controllers/user.controllers')
+const LoginController = require('../controllers/login.controllers')
 const upload = require('../multer')
 const passport = require('passport');
 
+
 module.exports = (app) => {
-    app.post('/register', upload.single('image'), UserController.createUser);
-    app.post('/login', passport.authenticate('local', {session:false}), UserController.login);
+    app.post('/register', upload.single('image'), LoginController.createUser);
+    app.post('/login', passport.authenticate('local', {session:false}), LoginController.login);
+    app.post('/logout', passport.authenticate('jwt', {session:false}), LoginController.logout);
     app.post('/api/menuitem', upload.single('image'), UserController.createMenuItem);
     app.get ('/api/users/:id',UserController.getOneUser);
     // app.put('/api/users/:id',UserController.updateUser);
