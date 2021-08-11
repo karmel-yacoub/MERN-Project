e5c2a19bd917dea606d3d9c0f856b95c63a9a622
unknown <khalilayyash@gmail.com>
2021-08-11 09:48:26 +0300

    auth

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,14 @@
 const {User} =require('../models/user.models')
+const passport = require('passport');
+const passportConfig = require('../passport');
+const JWT = require('jsonwebtoken');
+
+const signToken = userID => {
+    return JWT.sign({
+        iss: "khalil",
+        sub : userID,
+    }, "khalil", {expiresIn: "1h"});
+}
 
 // import controller Image saving in Database
 var fs = require('fs');
@@ -13,10 +23,23 @@ module.exports.createUser=(req,res) => {
         phone,
         location,
         genre,
-        picture :{
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-            contentType: 'image/png'
-        },
+        // picture :{
+        //     data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+        //     contentType: 'image/png'
+        // },
         menu,
     })
+    .then(newUser => res.json(newUser))
+    .catch(err => res.json(err));
+}
+
+module.exports.login = (req, res) => {
+    console.log("login....")
+    console.log(req)
+    if(req.isAuthenticated()) {
+        const {_id, name, genre} = req.user;
+        const token = signToken(_id);
+        res.cookie('access_token', token, {httpOnly: true, sameSite: true});
+        res.status(200).json({isAuthenticated: true, user: {name, genre,k:"sds"}});
+    }
 }
