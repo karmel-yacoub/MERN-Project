787bd34a51a830192b023f41e76a94cdee3e688c
karmel yacoub <johndoe@example.com>
2021-08-11 09:55:49 +0300

    Merge branch 'master' of https://github.com/karmel-yacoub/MERN-Project

--- server/controllers/menuItemControllers.js
+++ server/controllers/menuItemControllers.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 module.exports.createMenuItem=(req,res) => {
     const{name , price , description,picture } = req.body;
-    User.create({
+    MenuItem.create({
         name,
         price,
         description,
@@ -15,4 +15,27 @@ module.exports.createMenuItem=(req,res) => {
             contentType: 'image/png'
         }
     })
+    .then(MenuItems => res.json(MenuItems ))
+    .catch(err => res.json(err))
 }
+    module.exports.findAllItems = (req,res)=>{
+        MenuItem.find({})
+        .then(MenuItems => res.json(MenuItems ))
+        .catch(err => res.json(err))
+     
+    }
+     module.exports.findOneSingleItem = (req, res)=> {
+        MenuItem.findOne({_id: req.params.id})
+        .then(oneSingleItem => res.json(oneSingleItem))
+        .catch(err => res.json({message: "something went wrong", error: err}));
+     }
+     module.exports.ubdateItem =(req, res)=> {
+        MenuItem.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+            .then(updatedMeal=> res.json(updatedMeal))
+            .catch(err => res.json(err))
+    }
+    module.exports.deleteItem =(req, res) =>{
+        MenuItem.deleteOne({ _id: req.params.id })
+        .then(deletedMeal => res.json(deletedMeal))
+        .catch(err => res.json(err))
+    } 
