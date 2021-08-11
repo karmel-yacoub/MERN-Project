941df5ac27d4c596083c8d5c50eec99730964f16
karmel yacoub <johndoe@example.com>
2021-08-11 13:34:49 +0300

    user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,17 +6,11 @@ module.exports = (app) => {
     app.post('/register', upload.single('image'), UserController.createUser);
     app.post('/login', passport.authenticate('local', {session:false}), UserController.login);
     app.post('/api/menuitem', upload.single('image'), UserController.createMenuItem);
-<<<<<<< HEAD
     app.get ('/api/users/:id',UserController.getOneUser);
     app.put('/api/users/:id',UserController.updateuser);
     app.delete('/api/users/:id', UserController.deleteUser);
+    app.post('/api/order',UserController.createOrder);
     
-=======
-    app.get ('/api/users',UserController.getAllUsers);
-    // app.get ('/api/users/:id',UserController.getOneUser);
-    // app.put('/api/users/:id',UserController.updateUser);
-    // app.delete('/api/users/:id', UserController.deleteUser);
->>>>>>> 63ae0403d06d90a050ed6731de28cb9a879b1e2e
 
     // app.post('/login', localF, UserController.login);
 
