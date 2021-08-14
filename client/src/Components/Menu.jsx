89fd228f73584fc822e5595c4842cd1410085077
Khalil-Abuayyash <khalilayyash.com>
2021-08-14 19:22:41 +0300

    menu

---
+++ client/src/Components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuItemForm from './MenuItemForm';
+
+const Menu = () => {
+    return (
+        <div>
+            <MenuItemForm />
+        </div>
+    )
+}
+
+export default Menu
---
+++ client/src/Components/MenuItemCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuItemCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MenuItemCard
